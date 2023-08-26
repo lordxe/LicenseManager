72e11772b203d6852affc2ed319df5a23011ad10
Kevin Daniel Cabello Halyburton <keving758@gmail.com>
2023-08-25 21:09:56 -0400

    First Commit

---
+++ src/main/java/cl/lordxe/LicenseManager/LicenseManagerApplication.java
@@ -0,0 +1,13 @@
+package cl.lordxe.LicenseManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseManagerApplication.class, args);
+	}
+
+}
